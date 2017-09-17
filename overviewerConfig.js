686ee503f629228e66d5ea2d19dffe0f26279695
jordanprince <32041781+jordanprince@users.noreply.github.com>
2017-09-17 20:52:35 +0100

    1

---
+++ overviewerConfig.js
@@ -0,0 +1,65 @@
+var overviewerConfig = {
+    "worlds": [
+        "survival - overworld"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                -216, 
+                64, 
+                228
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1505505645, 
+            "name": "Survival Overworld", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 13, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "survival - overworld", 
+            "maxZoom": 13, 
+            "path": "survival", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1505512168"
+    }
+};
